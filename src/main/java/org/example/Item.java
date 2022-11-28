26c53ca203a45f48d9d9bf3a9b3aeb1bde8ea426
Frode Lien <79.frode@gmail.com>
2022-11-28 11:58:22 +0100

    enum added

--- src/main/java/org/example/Item.java
+++ src/main/java/org/example/Item.java
@@ -1,5 +1,19 @@
 package org.example;
 
 public class Item {
+private String name;
+private ItemType type;
+private double price;
 
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void printItem(){
+        System.out.println(type);
+    }
 }
---
+++ src/main/java/org/example/ItemType.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public enum ItemType {
+    BURGER,
+    DRINK,
+    SIDE,
+    TOPPING
+}
